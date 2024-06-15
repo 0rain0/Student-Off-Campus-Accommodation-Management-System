8e38c179a0d1fdd26e2d838b6d2d07185aa31f94
penguin72487 <agar.io6100@gmail.com>
2024-06-15 15:24:15 +0800

    feat: Add account types and update VSS component
    The code changes include adding account types (管理员, 房东, 老师, 学生) and updating the VSS component to include buttons for checking student and class status.
    
    Recent user commits:
    - 修正新增Class的起始編號
    - Update SOCAMS2.sql
    - 修改班級刪除功能的錯誤
    
    Recent repository commits:
    - chore: Add .env to .gitignore
    - chore: Remove unnecessary database configuration file
    - 完成頁面跳轉 (#5)

---
+++ front-end/src/components/CheckClassStatus.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="common-layout">
+            <el-header id="header" style="display: flex;">
+                <el-page-header @click="router.push('/menu');">
+                    <!-- <template #breadcrumb>
+                        <el-breadcrumb separator="/">
+                            <el-breadcrumb-item :to="{ path: './menu' }">
+                                Menu
+                            </el-breadcrumb-item>
+                            <el-breadcrumb-item>系統管理</el-breadcrumb-item>
+                        </el-breadcrumb>
+                    </template> -->
+                    <template #content>
+                        <span class="text-large
+                        font-600 mr-3" style="color: white;"> 學生訪視 </span>
+                    </template>
+                </el-page-header>
+            </el-header>
+        </div>
+</template>
